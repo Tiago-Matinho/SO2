7cef749482dc55635bcf13f42b66d70a45a39450
Tiago Matinho <tiagomartinho97@gmail.com>
2020-06-26 19:55:56 +0100

    Base

---
+++ comunidade/src/test/java/so2/comunidade/ComunidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package so2.comunidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComunidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
