ae99739ae7ea75309949c0216463693a1b9aa54c
Tiago Matinho <tiagomartinho97@gmail.com>
2020-06-27 18:20:31 +0100

    home

--- so2-t02-35735-39996/comunidade/src/main/java/so2/comunidade/controllers/HomeController.java
+++ so2-t02-35735-39996/comunidade/src/main/java/so2/comunidade/controllers/HomeController.java
@@ -3,11 +3,14 @@ package so2.comunidade.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+
 @Controller
-public class WelcomeController {
+class HomeController {
 
     @GetMapping("/")
-    public String welcome() {
-        return "welcome";
+    String index(Principal principal) {
+        return principal != null ? "home/homeSignedIn" : "home/homeNotSignedIn";
     }
+
 }
