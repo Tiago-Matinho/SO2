2ca78e3089150815792028ecf2ea1f6f38e3dd31
Tiago Matinho <tiagomartinho97@gmail.com>
2020-06-28 19:59:22 +0100

    update

---
+++ so2-t02-35735-39996/comunidade/src/main/java/so2/comunidade/SecurityManager.java
@@ -0,0 +1,9 @@
+package so2.comunidade;
+
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityManager extends WebSecurityConfigurerAdapter {
+
+    
+}
---
+++ so2-t02-35735-39996/comunidade/src/main/java/so2/comunidade/services/RegistoService.java
@@ -0,0 +1,34 @@
+package so2.comunidade.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import so2.comunidade.dados.Registo;
+import so2.comunidade.repository.RegistoRepository;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+
+@Service
+public class RegistoService {
+
+    @Autowired
+    private RegistoRepository repository;
+
+    public List<Registo> getAllRegisto() {
+        return this.repository.findAll();
+    }
+
+    public List<Registo> findByEspacoAndDateAfter(long espaco) {
+        TimeZone tz = TimeZone.getTimeZone("Portugal");
+        Calendar hora_acores = Calendar.getInstance(tz);
+        hora_acores.add(Calendar.HOUR, -1);
+
+        return repository.findByEspacoAndDateAfter(espaco, hora_acores.getTime());
+    }
+
+    public Registo findById(long id) {
+        return this.repository.findById(id);
+    }
+}
