02dc9fd2a7a509c3b29836c7de90eb860f1d1030
Tiago Matinho <tiagomartinho97@gmail.com>
2020-06-29 13:44:11 +0100

    update

--- so2-t02-35735-39996/comunidade/src/main/java/so2/comunidade/controllers/HomeController.java
+++ so2-t02-35735-39996/comunidade/src/main/java/so2/comunidade/controllers/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping(value="/")
-    public static String welcome() {
+    public static String home() {
         return "home";
     }
 
