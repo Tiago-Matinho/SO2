08d47af6c61e5885f18c2e7ee296684a5ee7ff56
Tiago Matinho <tiagomartinho97@gmail.com>
2020-07-08 21:15:07 +0100

    entrega

--- so2-t02-35735-39996/comunidade/src/main/java/so2/comunidade/security/WebSecurityConfig.java
+++ so2-t02-35735-39996/comunidade/src/main/java/so2/comunidade/security/WebSecurityConfig.java
@@ -40,7 +40,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-            .antMatchers("/", "/home", "/join", "/registo/*", "/registo/nome/*").permitAll()
+            .antMatchers("/", "/home", "/join").permitAll()
             .anyRequest().authenticated()
             .and()
             .formLogin()
